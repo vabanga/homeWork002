bc3fd24e44bc11882bc68d0b4597bd6620f31937
vabanga <gohavorsin@gmail.com>
2017-07-13 04:11:24 +1000

    AlgorithmPHP.php

--- AlgorithmPHP.php
+++ AlgorithmPHP.php
@@ -5,6 +5,11 @@
     <title>Алгоритм на PHP</title>
 </head>
 <body>
+<form>
+    <p><b>Введите число:</b><br>
+    <input type="text" size="40">
+    <p><input type="submit" value="Отправить"></p>
+</form>
 
 </body>
 </html>
