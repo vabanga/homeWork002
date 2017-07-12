aa703445cb59e5bed35e4fc35b56f84885bfd7a0
vabanga <gohavorsin@gmail.com>
2017-07-13 04:57:27 +1000

    change 1

--- AlgorithmPHP.php
+++ AlgorithmPHP.php
@@ -5,11 +5,32 @@
     <title>Алгоритм на PHP</title>
 </head>
 <body>
-<form>
+<form action= "AlgorithmPHP.php" method= "POST">
     <p><b>Введите число:</b><br>
-    <input type="text" size="40">
+    <input type="text" name="integer" size="40">
     <p><input type="submit" value="Отправить"></p>
 </form>
+
+<?php
+
+$integer=$_POST['integer'];
+
+$a = 1;
+$b = 1;
+$p = $integer;
+
+if ($a > $p) {
+    if ($a == $p) {
+        echo "задуманное число входит в числовой ряд";
+    } else {
+        
+    }
+} else {
+    echo "задуманное число НЕ входит в числовой ряд";
+}
+
+
+?>
 
 </body>
 </html>
